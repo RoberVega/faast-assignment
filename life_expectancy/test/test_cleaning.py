80e5e7505c24bf9ae2a75b76c9bb3928808a62a4
Roberto Vega <rober.vega93@gmail.com>
2022-11-02 12:43:52 +0000

    "Changes to the testing according to assignment3"

--- life_expectancy/test/test_cleaning.py
+++ life_expectancy/test/test_cleaning.py
@@ -1,17 +1,21 @@
 """Tests for the cleaning module"""
 import pandas as pd
+import pytest
 
 from life_expectancy.cleaning import clean_data
-from life_expectancy.cleaning import load_data
 from . import OUTPUT_DIR
 
+@pytest.fixture(scope="session")
+def pt_life_expectancy_raw() -> pd.DataFrame:
+    """Fixture to load the raw data and use it for the function"""
+    filepath = OUTPUT_DIR / "eu_life_expectancy_raw.tsv"
+    return pd.read_csv(filepath, sep="\t")
 
-def test_clean_data(pt_life_expectancy_expected):
+
+def test_clean_data(pt_life_expectancy_expected, pt_life_expectancy_raw: pd.DataFrame):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data(load_data(),"PT")
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
+    pt_life_expectancy_actual = clean_data(pt_life_expectancy_raw,"PT").reset_index(drop=True)
+
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
     )
